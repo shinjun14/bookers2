eaceb1adb1ad96f262a613d143faf9c2ba878103
shinjun14 <shinjunsakano@gmail.com>
2021-09-19 09:20:58 +0000

    [Finish]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :image)
   end
 
 end
