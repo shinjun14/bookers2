a02e90f4f77bd1e718fb524bf87c0d82b6714ef7
shinjun14 <shinjunsakano@gmail.com>
2021-10-11 06:33:57 +0000

    課題６

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,22 +1,22 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @comment = current_user.post_comments.new(post_comment_params)
-    @comment.book_id = book.id
-    @book = book
+    @comment.book_id = @book.id
     @comment.save
   end
 
   def destroy
-    PostComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @comment = PostComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
   end
 
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment).merge(user_id: current_user.id, book_id: params[:book_id])
+    params.require(:post_comment).permit(:comment)
   end
 
 end
